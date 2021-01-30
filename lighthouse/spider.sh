3d6f711bcdf4b8425e1767579d6c0f06901b06e8
Jens Dibbern <jens.dibbern@gmail.com>
2021-01-30 12:04:25 +0100

    Change to Ubuntu latest and Google Chrome stable

--- lighthouse/spider.sh
+++ lighthouse/spider.sh
@@ -8,7 +8,7 @@ apt-get --only-upgrade install google-chrome-stable > /dev/null
 cd /lhbatch
 
 wget -r --spider --delete-after --force-html -l $DEPTH $SITE 2>&1 \
-    | grep '^--' | awk '{ print $3 }' | grep -v '\.\(css\|js\|svg\|ttf\|txt\|png\|gif\|xml\|woff2\|jpg\)$' \
+    | grep '^--' | awk '{ print $3 }' | grep -v '\.\(css\|js\|svg\|ttf\|txt\|png\|gif\|xml\|json\|ico\|otf\|woff\|woff2\|jpg\)$' \
     | grep -v -F '?' | sort | uniq > $OUTPUT
 
 lighthouse-batch -f $OUTPUT --no-report --params "--screenEmulation.disabled --form-factor=\"desktop\" --throttling.rttMs=40  --throttling.throughputKbps=10240  --throttling.requestLatencyMs=0  --throttling.downloadThroughputKbps=0  --throttling.uploadThroughputKbps=0  --throttling.cpuSlowdownMultiplier=1 --no-emulatedUserAgent" > lh.log 2>&1

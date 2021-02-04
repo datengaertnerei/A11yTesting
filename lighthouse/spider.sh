30aba5683b49e3f4b85a86b4286bc297bcc335f6
Jens Dibbern <jens.dibbern@gmail.com>
2021-02-04 10:32:44 +0100

    back to default presets

--- lighthouse/spider.sh
+++ lighthouse/spider.sh
@@ -11,6 +11,6 @@ wget -r --spider --delete-after --force-html -l $DEPTH $SITE 2>&1 \
     | grep '^--' | awk '{ print $3 }' | grep -v '\.\(css\|js\|svg\|ttf\|txt\|png\|gif\|xml\|json\|ico\|otf\|woff\|woff2\|jpg\)$' \
     | grep -v -F '?' | sort | uniq > $OUTPUT
 
-lighthouse-batch -f $OUTPUT --no-report --params "--screenEmulation.disabled --form-factor=\"desktop\" --throttling.rttMs=40  --throttling.throughputKbps=10240  --throttling.requestLatencyMs=0  --throttling.downloadThroughputKbps=0  --throttling.uploadThroughputKbps=0  --throttling.cpuSlowdownMultiplier=1 --no-emulatedUserAgent" > lh.log 2>&1
+lighthouse-batch -f $OUTPUT --no-report > lh.log 2>&1
 json2csv -i report/lighthouse/summary.json -o report/lighthouse/summary.csv --flatten-objects
 cp report/lighthouse/summary.csv /dev/stdout
